da06526bf5dd68b0c0bc7e878e5c2ed177363bfb
PeterMaZep <110669701+PeterMaZep@users.noreply.github.com>
2026-06-06 10:00:07 +0800

    Update stats.go

--- environment/docker/stats.go
+++ environment/docker/stats.go
@@ -10,11 +10,9 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/goccy/go-json"
 
-	"github.com/pelican-dev/wings/environment"
+	"github.com/pwindows/phantom-wings/environment"
 )
 
-// Uptime returns the current uptime of the container in milliseconds. If the
-// container is not currently running this will return 0.
 func (e *Environment) Uptime(ctx context.Context) (int64, error) {
 	ins, err := e.ContainerInspect(ctx)
 	if err != nil {
@@ -30,8 +28,6 @@ func (e *Environment) Uptime(ctx context.Context) (int64, error) {
 	return time.Since(started).Milliseconds(), nil
 }
 
-// Attach to the instance and then automatically emit an event whenever the resource usage for the
-// server process changes.
 func (e *Environment) pollResources(ctx context.Context) error {
 	if e.st.Load() == environment.ProcessOfflineState {
 		return errors.New("cannot enable resource polling on a stopped server")
@@ -67,7 +63,6 @@ func (e *Environment) pollResources(ctx context.Context) error {
 				return nil
 			}
 
-			// Disable collection if the server is in an offline state and this process is still running.
 			if e.st.Load() == environment.ProcessOfflineState {
 				e.log().Debug("process in offline state while resource polling is still active; stopping poll")
 				return nil
@@ -95,51 +90,29 @@ func (e *Environment) pollResources(ctx context.Context) error {
 	}
 }
 
-// The "docker stats" CLI call does not return the same value as the types.MemoryStats.Usage
-// value which can be rather confusing to people trying to compare panel usage to
-// their stats output.
-//
-// This math is from their CLI repository in order to show the same values to avoid people
-// bothering me about it. It should also reflect a slightly more correct memory value anyways.
-//
-// @see https://github.com/docker/cli/blob/96e1d1d6/cli/command/container/stats_helpers.go#L227-L249
 func calculateDockerMemory(stats container.MemoryStats) uint64 {
 	if v, ok := stats.Stats["total_inactive_file"]; ok && v < stats.Usage {
 		return stats.Usage - v
 	}
-
 	if v := stats.Stats["inactive_file"]; v < stats.Usage {
 		return stats.Usage - v
 	}
-
 	return stats.Usage
 }
 
-// Calculates the absolute CPU usage used by the server process on the system, not constrained
-// by the defined CPU limits on the container.
-//
-// @see https://github.com/docker/cli/blob/aa097cf1aa19099da70930460250797c8920b709/cli/command/container/stats_helpers.go#L166
 func calculateDockerAbsoluteCpu(pStats container.CPUStats, stats container.CPUStats) float64 {
-	// Calculate the change in CPU usage between the current and previous reading.
 	cpuDelta := float64(stats.CPUUsage.TotalUsage) - float64(pStats.CPUUsage.TotalUsage)
-
-	// Calculate the change for the entire system's CPU usage between current and previous reading.
 	systemDelta := float64(stats.SystemUsage) - float64(pStats.SystemUsage)
-
-	// Calculate the total number of CPU cores being used.
 	cpus := float64(stats.OnlineCPUs)
 	if cpus == 0.0 {
 		cpus = float64(len(stats.CPUUsage.PercpuUsage))
 	}
-
 	percent := 0.0
 	if systemDelta > 0.0 && cpuDelta > 0.0 {
 		percent = (cpuDelta / systemDelta) * 100.0
-
 		if cpus > 0 {
 			percent *= cpus
 		}
 	}
-
 	return math.Round(percent*1000) / 1000
 }
