0f02eb97f0ccddb1ed5d7ee3ef86b1e0a05a6ae8
PeterMaZep <110669701+PeterMaZep@users.noreply.github.com>
2026-06-06 15:10:46 +0800

    Update parser.go

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"io"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -20,11 +21,9 @@ import (
 	"gopkg.in/ini.v1"
 	"gopkg.in/yaml.v3"
 
-	"github.com/pelican-dev/wings/config"
-	"github.com/pelican-dev/wings/internal/ufs"
+	"github.com/pwindows/phantom-wings/config"
 )
 
-// The file parsing options that are available for a server configuration file.
 const (
 	File       = "file"
 	Yaml       = "yaml"
@@ -40,26 +39,14 @@ type ReplaceValue struct {
 	valueType jsonparser.ValueType
 }
 
-// Value returns the underlying value of the replacement. Be aware that this
-// can include escaped UTF-8 sequences that will need to be handled by the caller
-// in order to avoid accidentally injecting invalid sequences into the running
-// process.
-//
-// For example the expected value may be "§Foo" but you'll be working directly
-// with "\u00a7FOo" for this value. This will cause user pain if not solved since
-// that is clearly not the value they were expecting to be using.
 func (cv *ReplaceValue) Value() []byte {
 	return cv.value
 }
 
-// Type returns the underlying data type for the Value field.
 func (cv *ReplaceValue) Type() jsonparser.ValueType {
 	return cv.valueType
 }
 
-// String returns the value as a string representation. This will automatically
-// handle casting the UTF-8 sequence into the expected value, switching something
-// like "\u00a7Foo" into "§Foo".
 func (cv *ReplaceValue) String() string {
 	switch cv.Type() {
 	case jsonparser.String:
@@ -105,23 +92,14 @@ func (cp ConfigurationParser) String() string {
 	return string(cp)
 }
 
-// ConfigurationFile defines a configuration file for the server startup. These
-// will be looped over and modified before the server finishes booting.
 type ConfigurationFile struct {
 	FileName        string                         `json:"file"`
 	Parser          ConfigurationParser            `json:"parser"`
 	Replace         []ConfigurationFileReplacement `json:"replace"`
-	AllowCreateFile bool                           `json:"create_file"` // assumed true by unmarshal as it was the original behaviour
-
-	// Tracks Wings' configuration so that we can quickly get values
-	// out of it when variables request it.
-	configuration []byte
+	AllowCreateFile bool                           `json:"create_file"`
+	configuration   []byte
 }
 
-// UnmarshalJSON is a custom unmarshaler for configuration files. If there is an
-// error while parsing out the replacements, don't fail the entire operation,
-// just log a global warning so someone can find the issue, and return an empty
-// array of replacements.
 func (f *ConfigurationFile) UnmarshalJSON(data []byte) error {
 	var m map[string]*json.RawMessage
 	if err := json.Unmarshal(data, &m); err != nil {
@@ -151,7 +129,6 @@ func (f *ConfigurationFile) UnmarshalJSON(data []byte) error {
 		}
 	}
 
-	// test if "create_file" exists, if not just assume true
 	if val, exists := m["create_file"]; exists && val != nil {
 		if err := json.Unmarshal(*val, &f.AllowCreateFile); err != nil {
 			log.WithField("file", f.FileName).WithField("error", err).Warn("create_file unmarshal failed")
@@ -165,27 +142,20 @@ func (f *ConfigurationFile) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-// ConfigurationFileReplacement defines a single find/replace instance for a
-// given server configuration file.
 type ConfigurationFileReplacement struct {
 	Match       string       `json:"match"`
 	IfValue     string       `json:"if_value"`
 	ReplaceWith ReplaceValue `json:"replace_with"`
 }
 
-// UnmarshalJSON handles unmarshaling the JSON representation into a struct that
-// provides more useful data to this functionality.
 func (cfr *ConfigurationFileReplacement) UnmarshalJSON(data []byte) error {
 	m, err := jsonparser.GetString(data, "match")
 	if err != nil {
 		return err
 	}
-
 	cfr.Match = m
 
 	iv, err := jsonparser.GetString(data, "if_value")
-	// We only check keypath here since match & replace_with should be present on all of
-	// them, however if_value is optional.
 	if err != nil && err != jsonparser.KeyPathNotFoundError {
 		return err
 	}
@@ -196,9 +166,6 @@ func (cfr *ConfigurationFileReplacement) UnmarshalJSON(data []byte) error {
 		if err != jsonparser.KeyPathNotFoundError {
 			return err
 		}
-
-		// Okay, likely dealing with someone who forgot to upgrade their eggs, so in
-		// that case, fallback to using the old key which was "value".
 		rw, dt, _, err = jsonparser.Get(data, "value")
 		if err != nil {
 			return err
@@ -229,12 +196,7 @@ func newTemplatableConfig(c *config.Configuration) templatableConfig {
 	return t
 }
 
-// Parse parses a given configuration file and updates all the values within
-// as defined in the API response from the Panel.
-func (f *ConfigurationFile) Parse(file ufs.File) error {
-	//log.WithField("path", path).WithField("parser", f.Parser.String()).Debug("parsing server configuration file")
-
-	// What the fuck is going on here?
+func (f *ConfigurationFile) Parse(file *os.File) error {
 	if mb, err := json.Marshal(newTemplatableConfig(config.Get())); err != nil {
 		return err
 	} else {
@@ -242,7 +204,6 @@ func (f *ConfigurationFile) Parse(file ufs.File) error {
 	}
 
 	var err error
-
 	switch f.Parser {
 	case Properties:
 		err = f.parsePropertiesFile(file)
@@ -262,15 +223,12 @@ func (f *ConfigurationFile) Parse(file ufs.File) error {
 	return err
 }
 
-// Parses an xml file.
-func (f *ConfigurationFile) parseXmlFile(file ufs.File) error {
+func (f *ConfigurationFile) parseXmlFile(file *os.File) error {
 	doc := etree.NewDocument()
 	if _, err := doc.ReadFrom(file); err != nil {
 		return err
 	}
 
-	// If there is no root we should create a basic start to the file. This isn't required though,
-	// and if it doesn't work correctly I'll just remove the code.
 	if doc.Root() == nil {
 		doc.CreateProcInst("xml", `version="1.0" encoding="utf-8"`)
 	}
@@ -281,8 +239,6 @@ func (f *ConfigurationFile) parseXmlFile(file ufs.File) error {
 			return err
 		}
 
-		// If this is the first item and there is no root element, create that root now and apply
-		// it for future use.
 		if i == 0 && doc.Root() == nil {
 			parts := strings.SplitN(replacement.Match, ".", 2)
 			doc.SetRoot(doc.CreateElement(parts[0]))
@@ -290,18 +246,9 @@ func (f *ConfigurationFile) parseXmlFile(file ufs.File) error {
 
 		path := "./" + strings.Replace(replacement.Match, ".", "/", -1)
 
-		// If we're not doing a wildcard replacement go ahead and create the
-		// missing element if we cannot find it yet.
 		if !strings.Contains(path, "*") {
 			parts := strings.Split(replacement.Match, ".")
-
-			// Set the initial element to be the root element, and then work from there.
 			element := doc.Root()
-
-			// Iterate over the path to create the required structure for the given element's path.
-			// This does not set a value, only ensures that the base structure exists. We start at index
-			// 1 because an XML document can only contain a single root element, and from there we'll
-			// work our way down the chain.
 			for _, tag := range parts[1:] {
 				if e := element.FindElement(tag); e == nil {
 					element = element.CreateElement(tag)
@@ -311,12 +258,10 @@ func (f *ConfigurationFile) parseXmlFile(file ufs.File) error {
 			}
 		}
 
-		// Iterate over the elements we found and update their values.
 		for _, element := range doc.FindElements(path) {
 			if xmlValueMatchRegex.MatchString(value) {
 				k := xmlValueMatchRegex.ReplaceAllString(value, "$1")
 				v := xmlValueMatchRegex.ReplaceAllString(value, "$2")
-
 				element.RemoveAttr(k)
 				element.CreateAttr(k, v)
 			} else {
@@ -332,25 +277,19 @@ func (f *ConfigurationFile) parseXmlFile(file ufs.File) error {
 		return err
 	}
 
-	// Ensure the XML is indented properly.
 	doc.Indent(2)
-
-	// Write the XML to the file.
 	if _, err := doc.WriteTo(file); err != nil {
 		return err
 	}
 	return nil
 }
 
-// Parses an ini file.
-func (f *ConfigurationFile) parseIniFile(file ufs.File) error {
-	// Wrap the file in a NopCloser so the ini package doesn't close the file.
+func (f *ConfigurationFile) parseIniFile(file *os.File) error {
 	cfg, err := ini.Load(io.NopCloser(file))
 	if err != nil {
 		return err
 	}
 
-	// Force format to be key = value
 	ini.PrettyFormat = false
 	ini.PrettyEqual = true
 
@@ -386,14 +325,11 @@ func (f *ConfigurationFile) parseIniFile(file ufs.File) error {
 
 		k := path[0]
 		s := cfg.Section("")
-		// Passing a key of foo.bar will look for "bar" in the "[foo]" section of the file.
 		if len(path) == 2 {
 			k = path[1]
 			s = cfg.Section(path[0])
 		}
 
-		// If no section was found, create that new section now and then set the
-		// section value we're using to be the new one.
 		if s == nil {
 			s, err = cfg.NewSection(path[0])
 			if err != nil {
@@ -401,8 +337,6 @@ func (f *ConfigurationFile) parseIniFile(file ufs.File) error {
 			}
 		}
 
-		// If the key exists in the file go ahead and set the value, otherwise try to
-		// create it in the section.
 		if s.HasKey(k) {
 			s.Key(k).SetValue(value)
 		} else {
@@ -418,17 +352,13 @@ func (f *ConfigurationFile) parseIniFile(file ufs.File) error {
 	if err := file.Truncate(0); err != nil {
 		return err
 	}
-
 	if _, err := cfg.WriteTo(file); err != nil {
 		return err
 	}
 	return nil
 }
 
-// Parses a json file updating any matching key/value pairs. If a match is not found, the
-// value is set regardless in the file. See the commentary in parseYamlFile for more details
-// about what is happening during this process.
-func (f *ConfigurationFile) parseJsonFile(file ufs.File) error {
+func (f *ConfigurationFile) parseJsonFile(file *os.File) error {
 	b, err := io.ReadAll(file)
 	if err != nil {
 		return err
@@ -459,9 +389,7 @@ func (f *ConfigurationFile) parseJsonFile(file ufs.File) error {
 	return nil
 }
 
-// Parses a yaml file and updates any matching key/value pairs before persisting
-// it back to the disk.
-func (f *ConfigurationFile) parseYamlFile(file ufs.File) error {
+func (f *ConfigurationFile) parseYamlFile(file *os.File) error {
 	b, err := io.ReadAll(file)
 	if err != nil {
 		return err
@@ -472,16 +400,11 @@ func (f *ConfigurationFile) parseYamlFile(file ufs.File) error {
 		return err
 	}
 
-	// Unmarshal the yaml data into a JSON interface such that we can work with
-	// any arbitrary data structure. This allows us to use gjson/sjson for
-	// working with unknown JSON significantly easier.
 	jsonBytes, err := json.Marshal(dyno.ConvertMapI2MapS(i))
 	if err != nil {
 		return err
 	}
 
-	// Now that the data is converted, treat it just like JSON and pass it to the
-	// iterator function to update values as necessary.
 	data, err := f.IterateOverJson(jsonBytes)
 	if err != nil {
 		return err
@@ -506,17 +429,13 @@ func (f *ConfigurationFile) parseYamlFile(file ufs.File) error {
 	if err := file.Truncate(0); err != nil {
 		return err
 	}
-
-	// Write the data to the file.
 	if _, err := io.Copy(file, bytes.NewReader(marshaled)); err != nil {
 		return errors.Wrap(err, "parser: failed to write properties file to disk")
 	}
 	return nil
 }
 
-// Parses a toml file and updates any matching key/value pairs before persisting
-// it back to the disk.
-func (f *ConfigurationFile) parseTomlFile(file ufs.File) error {
+func (f *ConfigurationFile) parseTomlFile(file *os.File) error {
 	b, err := io.ReadAll(file)
 	if err != nil {
 		return err
@@ -527,7 +446,6 @@ func (f *ConfigurationFile) parseTomlFile(file ufs.File) error {
 		return err
 	}
 
-	// Convert to JSON to reuse IterateOverJson for value replacement.
 	jsonBytes, err := json.Marshal(dyno.ConvertMapI2MapS(i))
 	if err != nil {
 		return err
@@ -546,7 +464,6 @@ func (f *ConfigurationFile) parseTomlFile(file ufs.File) error {
 	}
 	jsonData = normalizeTomlTypes(jsonData)
 
-	// Remarshal back to TOML format.
 	marshaled, err := toml.Marshal(jsonData)
 	if err != nil {
 		return err
@@ -558,16 +475,12 @@ func (f *ConfigurationFile) parseTomlFile(file ufs.File) error {
 	if err := file.Truncate(0); err != nil {
 		return err
 	}
-
 	if _, err := io.Copy(file, bytes.NewReader(marshaled)); err != nil {
 		return errors.Wrap(err, "parser: failed to write toml file to disk")
 	}
 	return nil
 }
 
-// normalizeYamlTypes converts json.Number values (produced by UseNumber()) into
-// proper Go numeric types so that yaml.Marshal writes integers as plain integers
-// instead of float64 scientific notation (e.g. 1.5e+18 for large Snowflake IDs).
 func normalizeYamlTypes(value interface{}) interface{} {
 	switch typed := value.(type) {
 	case map[string]interface{}:
@@ -582,8 +495,6 @@ func normalizeYamlTypes(value interface{}) interface{} {
 		return typed
 	case json.Number:
 		s := typed.String()
-		// Preserve float representation: if the number contains '.', 'e' or 'E'
-		// it was originally a float and must not be coerced to int64.
 		if strings.ContainsAny(s, ".eE") {
 			if floatVal, err := typed.Float64(); err == nil {
 				return floatVal
@@ -635,10 +546,7 @@ func normalizeTomlTypes(value interface{}) interface{} {
 	}
 }
 
-// Parses a text file using basic find and replace. This is a highly inefficient method of
-// scanning a file and performing a replacement. You should attempt to use anything other
-// than this function where possible.
-func (f *ConfigurationFile) parseTextFile(file ufs.File) error {
+func (f *ConfigurationFile) parseTextFile(file *os.File) error {
 	b := bytes.NewBuffer(nil)
 	s := bufio.NewScanner(file)
 	var replaced bool
@@ -646,13 +554,9 @@ func (f *ConfigurationFile) parseTextFile(file ufs.File) error {
 		line := s.Bytes()
 		replaced = false
 		for _, replace := range f.Replace {
-			// If this line doesn't match what we expect for the replacement, move on to the next
-			// line. Otherwise, update the line to have the replacement value.
 			if !bytes.HasPrefix(line, []byte(replace.Match)) {
 				continue
 			}
-			// If an if_value is set, only replace when the remainder of the line matches.
-			// Trim trailing \r\n so Windows line endings do not break the comparison.
 			if replace.IfValue != "" {
 				remainder := bytes.TrimRight(bytes.TrimPrefix(line, []byte(replace.Match)), "\r\n")
 				if string(remainder) != replace.IfValue {
@@ -674,41 +578,13 @@ func (f *ConfigurationFile) parseTextFile(file ufs.File) error {
 	if err := file.Truncate(0); err != nil {
 		return err
 	}
-
-	// Write the data to the file.
 	if _, err := io.Copy(file, b); err != nil {
 		return errors.Wrap(err, "parser: failed to write properties file to disk")
 	}
 	return nil
 }
 
-// parsePropertiesFile parses a properties file and updates the values within it
-// to match those that are passed. Once completed the new file is written to the
-// disk. This will cause comments not present at the head of the file to be
-// removed unfortunately.
-//
-// Any UTF-8 value will be written back to the disk as their escaped value rather
-// than the raw value There is no winning with this logic. This fixes a bug where
-// users with hand rolled UTF-8 escape sequences would have all sorts of pain in
-// their configurations because we were writing the UTF-8 literal characters which
-// their games could not actually handle.
-//
-// However, by adding this fix to only store the escaped UTF-8 sequence we
-// unwittingly introduced a "regression" that causes _other_ games to have issues
-// because they can only handle the unescaped representations. I cannot think of
-// a simple approach to this problem that doesn't just lead to more complicated
-// cases and problems.
-//
-// So, if your game cannot handle parsing UTF-8 sequences that are escaped into
-// the string, well, sucks. There are fewer of those games than there are games
-// that have issues parsing the raw UTF-8 sequence into a string? Also how does
-// one really know what the user intended at this point? We'd need to know if
-// the value was escaped or not to begin with before setting it, which I suppose
-// can work but jesus that is going to be some annoyingly complicated logic?
-//
-// @see https://github.com/pterodactyl/panel/issues/2308 (original)
-// @see https://github.com/pterodactyl/panel/issues/3009 ("bug" introduced as result)
-func (f *ConfigurationFile) parsePropertiesFile(file ufs.File) error {
+func (f *ConfigurationFile) parsePropertiesFile(file *os.File) error {
 	b, err := io.ReadAll(file)
 	if err != nil {
 		return err
@@ -716,8 +592,6 @@ func (f *ConfigurationFile) parsePropertiesFile(file ufs.File) error {
 
 	s := bytes.NewBuffer(nil)
 	scanner := bufio.NewScanner(bytes.NewReader(b))
-	// Scan until we hit a line that is not a comment that actually has content
-	// on it. Keep appending the comments until that time.
 	for scanner.Scan() {
 		text := scanner.Bytes()
 		if len(text) > 0 && text[0] != '#' {
@@ -735,7 +609,6 @@ func (f *ConfigurationFile) parsePropertiesFile(file ufs.File) error {
 		return errors.Wrap(err, "parser: could not load properties file for configuration update")
 	}
 
-	// Replace any values that need to be replaced.
 	for _, replace := range f.Replace {
 		data, err := f.LookupConfigurationValue(replace)
 		if err != nil {
@@ -743,9 +616,6 @@ func (f *ConfigurationFile) parsePropertiesFile(file ufs.File) error {
 		}
 
 		v, ok := p.Get(replace.Match)
-		// Don't attempt to replace the value if we're looking for a specific value and
-		// it does not match. If there was no match at all in the file for this key but
-		// we're doing an IfValue match, do nothing.
 		if replace.IfValue != "" && (!ok || (ok && v != replace.IfValue)) {
 			continue
 		}
@@ -755,16 +625,11 @@ func (f *ConfigurationFile) parsePropertiesFile(file ufs.File) error {
 		}
 	}
 
-	// Add the new file content to the string builder.
 	for _, key := range p.Keys() {
 		value, ok := p.Get(key)
 		if !ok {
 			continue
 		}
-		// This escape is intentional!
-		//
-		// See the docblock for this function for more details, do not change this
-		// or you'll cause a flood of new issue reports no one wants to deal with.
 		s.WriteString(key + "=" + strings.Trim(strconv.QuoteToASCII(value), "\"") + "\n")
 	}
 
@@ -774,8 +639,6 @@ func (f *ConfigurationFile) parsePropertiesFile(file ufs.File) error {
 	if err := file.Truncate(0); err != nil {
 		return err
 	}
-
-	// Write the data to the file.
 	if _, err := io.Copy(file, s); err != nil {
 		return errors.Wrap(err, "parser: failed to write properties file to disk")
 	}
