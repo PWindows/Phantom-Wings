b28c4b2ceae70f929af4b01635280c7e8e17c80a
PeterMaZep <110669701+PeterMaZep@users.noreply.github.com>
2026-06-06 09:54:14 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -132,3 +132,405 @@ type CrashDetection struct {
 	DetectCleanExitAsCrash bool `default:"true" yaml:"detect_clean_exit_as_crash"`
 	Timeout                int  `default:"60" json:"timeout"`
 }
+
+type Backups struct {
+	WriteLimit                  int    `default:"0" yaml:"write_limit"`
+	CompressionLevel            string `default:"best_speed" yaml:"compression_level"`
+	RemoveBackupsOnServerDelete bool   `default:"true" yaml:"remove_backups_on_server_delete"`
+}
+
+type Transfers struct {
+	DownloadLimit int `default:"0" yaml:"download_limit"`
+}
+
+type ConsoleThrottles struct {
+	Enabled bool   `json:"enabled" yaml:"enabled" default:"true"`
+	Lines   uint64 `json:"lines" yaml:"lines" default:"2000"`
+	Period  uint64 `json:"line_reset_interval" yaml:"line_reset_interval" default:"100"`
+}
+
+type Token struct {
+	ID    string
+	Token string
+}
+
+type Configuration struct {
+	Token                    Token               `json:"-" yaml:"-"`
+	path                     string
+	Debug                    bool
+	AppName                  string              `default:"phantom" json:"app_name" yaml:"app_name"`
+	Uuid                     string
+	AuthenticationTokenId    string              `json:"token_id" yaml:"token_id"`
+	AuthenticationToken      string              `json:"token" yaml:"token"`
+	Api                      ApiConfiguration    `json:"api" yaml:"api"`
+	System                   SystemConfiguration `json:"system" yaml:"system"`
+	Docker                   DockerConfiguration `json:"docker" yaml:"docker"`
+	Throttles                ConsoleThrottles
+	PanelLocation            string                   `json:"-" yaml:"remote"`
+	RemoteQuery              RemoteQueryConfiguration `json:"remote_query" yaml:"remote_query"`
+	AllowedMounts            []string                 `json:"-" yaml:"allowed_mounts"`
+	SearchRecursion          SearchRecursion          `yaml:"Search"`
+	BlockBaseDirMount        bool                     `default:"true" json:"-" yaml:"BlockBaseDirMount"`
+	AllowedOrigins           []string                 `json:"allowed_origins" yaml:"allowed_origins"`
+	AllowCORSPrivateNetwork  bool                     `json:"allow_cors_private_network" yaml:"allow_cors_private_network"`
+	IgnorePanelConfigUpdates bool                     `json:"ignore_panel_config_updates" yaml:"ignore_panel_config_updates"`
+}
+
+type SearchRecursion struct {
+	BlacklistedDirs   []string `default:"[\"node_modules\", \".git\", \".wine\", \"appcache\", \"depotcache\", \"vendor\"]" yaml:"blacklisted_dirs" json:"blacklisted_dirs"`
+	MaxRecursionDepth int      `default:"8" yaml:"max_recursion_depth" json:"max_recursion_depth"`
+}
+
+func NewAtPath(path string) (*Configuration, error) {
+	var c Configuration
+	if err := defaults.Set(&c); err != nil {
+		return nil, err
+	}
+	c.path = path
+	return &c, nil
+}
+
+func Set(c *Configuration) {
+	mu.Lock()
+	defer mu.Unlock()
+	token := c.Token.Token
+	if token == "" {
+		c.Token.Token = c.AuthenticationToken
+		token = c.Token.Token
+	}
+	if _config == nil || _config.Token.Token != token {
+		_jwtAlgo = jwt.NewHS256([]byte(token))
+	}
+	_config = c
+}
+
+func SetDebugViaFlag(d bool) {
+	mu.Lock()
+	defer mu.Unlock()
+	_config.Debug = d
+	_debugViaFlag = d
+}
+
+func Get() *Configuration {
+	mu.RLock()
+	//goland:noinspection GoVetCopyLock
+	c := *_config
+	mu.RUnlock()
+	return &c
+}
+
+func Update(callback func(c *Configuration)) {
+	mu.Lock()
+	defer mu.Unlock()
+	callback(_config)
+}
+
+func GetJwtAlgorithm() *jwt.HMACSHA {
+	mu.RLock()
+	defer mu.RUnlock()
+	return _jwtAlgo
+}
+
+func WriteToDisk(c *Configuration) error {
+	_writeLock.Lock()
+	defer _writeLock.Unlock()
+	//goland:noinspection GoVetCopyLock
+	ccopy := *c
+	if _debugViaFlag {
+		ccopy.Debug = false
+	}
+	if c.path == "" {
+		return errors.New("cannot write configuration, no path defined in struct")
+	}
+	b, err := yaml.Marshal(&ccopy)
+	if err != nil {
+		return err
+	}
+	if err := os.WriteFile(c.path, b, 0o600); err != nil {
+		return err
+	}
+	return nil
+}
+
+func EnsurePelicanUser() error {
+	sysName, err := getSystemName()
+	if err != nil {
+		return err
+	}
+
+	if sysName == "distroless" {
+		_config.System.Username = system.FirstNotEmpty(os.Getenv("WINGS_USERNAME"), "phantom")
+		_config.System.User.Uid = system.MustInt(system.FirstNotEmpty(os.Getenv("WINGS_UID"), "988"))
+		_config.System.User.Gid = system.MustInt(system.FirstNotEmpty(os.Getenv("WINGS_GID"), "988"))
+		return nil
+	}
+
+	if _config.System.User.Rootless.Enabled {
+		log.Info("rootless mode is enabled, skipping user creation...")
+		u, err := user.Current()
+		if err != nil {
+			return err
+		}
+		_config.System.Username = u.Username
+		_config.System.User.Uid = system.MustInt(u.Uid)
+		_config.System.User.Gid = system.MustInt(u.Gid)
+		return nil
+	}
+
+	log.WithField("username", _config.System.Username).Info("checking for phantom system user")
+	u, err := user.Lookup(_config.System.Username)
+	if err != nil {
+		if _, ok := err.(user.UnknownUserError); !ok {
+			return err
+		}
+	} else {
+		_config.System.User.Uid = system.MustInt(u.Uid)
+		_config.System.User.Gid = system.MustInt(u.Gid)
+		return nil
+	}
+
+	command := fmt.Sprintf("useradd --system --no-create-home --shell /usr/sbin/nologin %s", _config.System.Username)
+	if strings.HasPrefix(sysName, "alpine") {
+		command = fmt.Sprintf("adduser -S -D -H -G %[1]s -s /sbin/nologin %[1]s", _config.System.Username)
+		if _, err := exec.Command("addgroup", "-S", _config.System.Username).Output(); err != nil {
+			return err
+		}
+	}
+
+	split := strings.Split(command, " ")
+	if _, err := exec.Command(split[0], split[1:]...).Output(); err != nil {
+		return err
+	}
+	u, err = user.Lookup(_config.System.Username)
+	if err != nil {
+		return err
+	}
+	_config.System.User.Uid = system.MustInt(u.Uid)
+	_config.System.User.Gid = system.MustInt(u.Gid)
+	return nil
+}
+
+func FromFile(path string) error {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	c, err := NewAtPath(path)
+	if err != nil {
+		return err
+	}
+	if err := yaml.Unmarshal(b, c); err != nil {
+		return err
+	}
+	c.Token = Token{
+		ID:    os.Getenv("WINGS_TOKEN_ID"),
+		Token: os.Getenv("WINGS_TOKEN"),
+	}
+	if c.Token.ID == "" {
+		c.Token.ID = c.AuthenticationTokenId
+	}
+	if c.Token.Token == "" {
+		c.Token.Token = c.AuthenticationToken
+	}
+	c.Token.ID, err = Expand(c.Token.ID)
+	if err != nil {
+		return err
+	}
+	c.Token.Token, err = Expand(c.Token.Token)
+	if err != nil {
+		return err
+	}
+	Set(c)
+	return nil
+}
+
+func ConfigureDirectories() error {
+	root := _config.System.RootDirectory
+	log.WithField("path", root).Debug("ensuring root data directory exists")
+	if err := os.MkdirAll(root, 0o700); err != nil {
+		return err
+	}
+	if d, err := filepath.EvalSymlinks(_config.System.Data); err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+	} else if d != _config.System.Data {
+		_config.System.Data = d
+	}
+	log.WithField("path", _config.System.Data).Debug("ensuring server data directory exists")
+	if err := os.MkdirAll(_config.System.Data, 0o700); err != nil {
+		return err
+	}
+	log.WithField("path", _config.System.TmpDirectory).Debug("ensuring temporary data directory exists")
+	if err := os.MkdirAll(_config.System.TmpDirectory, 0o700); err != nil {
+		return err
+	}
+	log.WithField("path", _config.System.ArchiveDirectory).Debug("ensuring archive data directory exists")
+	if err := os.MkdirAll(_config.System.ArchiveDirectory, 0o700); err != nil {
+		return err
+	}
+	log.WithField("path", _config.System.BackupDirectory).Debug("ensuring backup data directory exists")
+	if err := os.MkdirAll(_config.System.BackupDirectory, 0o700); err != nil {
+		return err
+	}
+	log.WithField("path", _config.System.User.Passwd.Directory).Debug("ensuring passwd directory exists")
+	if err := os.MkdirAll(_config.System.User.Passwd.Directory, 0o700); err != nil {
+		return err
+	}
+	log.WithField("path", _config.System.MachineID.Directory).Debug("ensuring machine-id directory exists")
+	if err := os.MkdirAll(_config.System.MachineID.Directory, 0o700); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ConfigurePasswd() (err error) {
+	if !_config.System.User.Passwd.Enable {
+		return
+	}
+	log.WithField("filepath", filepath.Join(_config.System.User.Passwd.Directory, "passwd")).
+		Debug("ensuring passwd file exists")
+	if err = os.WriteFile(filepath.Join(_config.System.User.Passwd.Directory, "passwd"),
+		[]byte(fmt.Sprintf("container:x:%d:%d::/home/container:/usr/sbin/nologin",
+			_config.System.User.Uid, _config.System.User.Gid)), 0644); err != nil {
+		return fmt.Errorf("could not write passwd file: %w", err)
+	}
+	log.WithField("filepath", filepath.Join(_config.System.User.Passwd.Directory, "group")).
+		Debug("ensuring group file exists")
+	if err = os.WriteFile(filepath.Join(_config.System.User.Passwd.Directory, "group"),
+		[]byte(fmt.Sprintf("container:x:%d:container",
+			_config.System.User.Gid)), 0644); err != nil {
+		return fmt.Errorf("could not write group file: %w", err)
+	}
+	return
+}
+
+func EnableLogRotation() error {
+	if !_config.System.EnableLogRotate {
+		log.Info("skipping log rotate configuration, disabled in wings config file")
+		return nil
+	}
+	if st, err := os.Stat("/etc/logrotate.d"); err != nil && !os.IsNotExist(err) {
+		return err
+	} else if (err != nil && os.IsNotExist(err)) || !st.IsDir() {
+		return nil
+	}
+	if _, err := os.Stat("/etc/logrotate.d/wings"); err == nil || !os.IsNotExist(err) {
+		return err
+	}
+	log.Info("no log rotation configuration found: adding file now")
+	f, err := os.Create("/etc/logrotate.d/wings")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	t, err := template.New("logrotate").Parse(`{{.LogDirectory}}/wings.log {
+    size 10M
+    compress
+    delaycompress
+    dateext
+    maxage 7
+    missingok
+    notifempty
+    postrotate
+        /usr/bin/systemctl kill -s HUP wings.service >/dev/null 2>&1 || true
+    endscript
+}`)
+	if err != nil {
+		return err
+	}
+	return errors.Wrap(t.Execute(f, _config.System), "config: failed to write logrotate to disk")
+}
+
+func (sc *SystemConfiguration) GetStatesPath() string {
+	return path.Join(sc.RootDirectory, "/states.json")
+}
+
+func ConfigureTimezone() error {
+	tz := os.Getenv("TZ")
+	if _config.System.Timezone == "" && tz != "" {
+		_config.System.Timezone = tz
+	}
+	if _config.System.Timezone == "" {
+		b, err := os.ReadFile("/etc/timezone")
+		if err != nil {
+			if !os.IsNotExist(err) {
+				return errors.WithMessage(err, "config: failed to open timezone file")
+			}
+			_config.System.Timezone = "UTC"
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+			out, err := exec.CommandContext(ctx, "timedatectl").Output()
+			if err != nil {
+				log.WithField("error", err).Warn("failed to execute \"timedatectl\" to determine system timezone, falling back to UTC")
+				return nil
+			}
+			r := regexp.MustCompile(`Time zone: ([\w/]+)`)
+			matches := r.FindSubmatch(out)
+			if len(matches) != 2 || string(matches[1]) == "" {
+				log.Warn("failed to parse timezone from \"timedatectl\" output, falling back to UTC")
+				return nil
+			}
+			_config.System.Timezone = string(matches[1])
+		} else {
+			_config.System.Timezone = string(b)
+		}
+	}
+	_config.System.Timezone = regexp.MustCompile(`(?i)[^a-z_/]+`).ReplaceAllString(_config.System.Timezone, "")
+	_, err := time.LoadLocation(_config.System.Timezone)
+	return errors.WithMessage(err, fmt.Sprintf("the supplied timezone %s is invalid", _config.System.Timezone))
+}
+
+func getSystemName() (string, error) {
+	release, err := osrelease.Read()
+	if err != nil {
+		return "", err
+	}
+	return release["ID"], nil
+}
+
+var (
+	openat2    atomic.Bool
+	openat2Set atomic.Bool
+)
+
+func UseOpenat2() bool {
+	if openat2Set.Load() {
+		return openat2.Load()
+	}
+	defer openat2Set.Store(true)
+	c := Get()
+	openatMode := c.System.OpenatMode
+	switch openatMode {
+	case "openat2":
+		openat2.Store(true)
+		return true
+	case "openat":
+		openat2.Store(false)
+		return false
+	default:
+		fd, err := unix.Openat2(unix.AT_FDCWD, "/", &unix.OpenHow{})
+		if err != nil {
+			log.WithError(err).Warn("error occurred while checking for openat2 support, falling back to openat")
+			openat2.Store(false)
+			return false
+		}
+		_ = unix.Close(fd)
+		openat2.Store(true)
+		return true
+	}
+}
+
+func Expand(v string) (string, error) {
+	v = os.ExpandEnv(v)
+	const filePrefix = "file://"
+	if strings.HasPrefix(v, filePrefix) {
+		p := v[len(filePrefix):]
+		b, err := os.ReadFile(p)
+		if err != nil {
+			return "", nil
+		}
+		v = string(bytes.TrimRight(bytes.TrimRight(b, "\r"), "\n"))
+	}
+	return v, nil
+}
